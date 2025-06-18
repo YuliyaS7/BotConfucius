9d7493d9767fb420fd200950cad36c54d5200797
Vasyl Kovalov <raid3r81@gmail.com>
2025-06-18 19:07:50 +0300

    Добавлен класс BotContext для работы с базой данных
    В коде добавлены директивы `using` для подключения необходимых пространств имен, таких как `Microsoft.EntityFrameworkCore`, `System`, `System.Collections.Generic`, `System.Linq`, `System.Text` и `System.Threading.Tasks`.
    
    Также был добавлен класс `BotContext`, который наследует от `DbContext`. В этом классе определен конструктор по умолчанию и конструктор, принимающий параметры `DbContextOptions<BotContext>`. Кроме того, добавлено свойство `DbSet<Quote> Quotes`, которое представляет коллекцию объектов `Quote` в контексте базы данных.

---
+++ DatabaseLibrary/Models/BotContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLibrary.Models;
+
+public class BotContext: DbContext
+{
+    public BotContext() : base() { }
+
+    public BotContext(DbContextOptions<BotContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Quote> Quotes { get; set; } = null!;
+}
