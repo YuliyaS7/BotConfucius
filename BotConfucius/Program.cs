718aef5f12201c1562836351f8869487b84f04e3
Vasyl Kovalov <raid3r81@gmail.com>
2025-06-18 20:52:57 +0300

    Telegram bot

--- BotConfucius/Program.cs
+++ BotConfucius/Program.cs
@@ -1,1 +1,55 @@
-﻿Console.WriteLine("Confucius bot start!");
+﻿using Telegram.Bot;
+
+// телеграм бот який відповідає на будь яке питання цитатами Конфуція
+
+var token = "7619230076:AAF0j_4Lbbwp8lKzgb8UZ1tJYx-A9f4Nxzg";
+var bot = new TelegramBotClient(token);
+
+//bot.OnMessage += Bot_OnMessage;
+var cts = new CancellationTokenSource();
+var cancelationToken = cts.Token;
+var receiverOptions = new Telegram.Bot.Polling.ReceiverOptions
+{
+    AllowedUpdates = Array.Empty<Telegram.Bot.Types.Enums.UpdateType>(), // Отримувати всі типи оновлень
+    DropPendingUpdates = true, // Скинути всі очікувані оновлення
+    Limit = 100 // Максимальна кількість оновлень за один запит
+};
+
+var me = await bot.GetMe(cancelationToken);
+Console.WriteLine($"Bot {me.FirstName} started");
+
+await bot.ReceiveAsync(
+    updateHandler: Bot_OnMessage, // Обробник повідомлень
+    errorHandler: null, // Тут можна вказати обробник помилок, якщо потрібно
+    receiverOptions: receiverOptions,
+    cancellationToken: cancelationToken
+);
+
+
+
+Console.ReadLine(); // Залишаємо програму запущеною, поки не буде натиснуто Enter
+
+cts.Cancel(); // Скасовуємо отримання оновлень при завершенні програми
+
+string GetRandomQuote()
+{
+    return "Це тестова цитата Конфуція."; // Тут має бути логіка для отримання випадкової цитати з бази даних
+
+    //using (var db = new DatabaseLibrary.Models.BotContext())
+    //{
+    //    var quotes = db.Quotes.ToList();
+    //    if (quotes.Count == 0)
+    //    {
+    //        return "No quotes available.";
+    //    }
+
+    //    var random = new Random();
+    //    int index = random.Next(quotes.Count);
+    //    return quotes[index].Text;
+    //}
+}
+
+async Task Bot_OnMessage(ITelegramBotClient bot, Telegram.Bot.Types.Update update, CancellationToken ct)
+{
+    await bot.SendMessage(update.Message.Chat.Id, GetRandomQuote());    
+}
