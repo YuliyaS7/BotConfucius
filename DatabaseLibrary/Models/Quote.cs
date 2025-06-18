c62099aa7421a7c061599bbc629a3ff0fe9caf61
Vasyl Kovalov <raid3r81@gmail.com>
2025-06-18 19:07:38 +0300

    Добавлен класс Quote с необходимыми свойствами
    В файле Quote.cs добавлены пространства имен и определен класс Quote.
    Свойства класса включают Id, Text (с ограничением длины до 1000 символов)
    и UpdatedAt, инициализируемое текущей датой и временем в формате UTC.

---
+++ DatabaseLibrary/Models/Quote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLibrary.Models;
+
+public class Quote
+{
+    public int Id { get; set; }
+    [MaxLength(1000)]
+    public string Text { get; set; } = string.Empty;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
